83dc91562ba43262a958f07d707307a1945048dc
matthieuGravy <matthieugravy@gmail.com>
2023-11-10 12:19:31 +0100

    content

---
+++ README.md
@@ -0,0 +1,55 @@
+# Multilingual HomePage
+## open source
+<img height="20px" src="https://img.shields.io/badge/Vue%20js-35495E?style=for-the-badge&logo=vuedotjs&logoColor=4FC08D" alt="vueJS" title="vueJS"/> <img height="20px" src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" alt="JS" title="JS"/> <img height="20px" src="https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white" alt="sass" title="sass"/> <img height="20px" src="https://img.shields.io/badge/json-5E5C5C?style=for-the-badge&logo=json&logoColor=white" alt="json" title="json"/>
+<br/>
+<br/>
+Multilingual HomePage: Dive into an immersive experience with our Vue 3 application, designed to offer a plethora of captivating features:
+
+- Harness the power of composition in Vue 3 to create a seamless and responsive application.
+- Immerse yourself in linguistic diversity through our custom JSON file, providing a smooth and accessible multilingual experience.
+- Astonish your visitors with smooth scroll animations, powered by the robust GSAP library.
+- Bring your site to life with innovative components that craft endless and memorable animations.
+- Enjoy a seamless user experience with a responsive design, ensuring your site is accessible at all times, regardless of the device used.
+
+Please note that our template is a work in progress, offering ample opportunities for improvement. Explore and customize it to create an even more exceptional web experience.
+
+> Created by _Matthieu Gravy_
+> > <a href="https://www.linkedin.com/in/matthieugravy/"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="linkedin" title="linkedin"/></a>
+<br/>
+<br/>
+<details>
+<summary> Usage Recommendations 
+</summary>
+
+<h4>Recommended IDE Setup</h4>
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+<h4>Customize configuration</h4> 
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+### Project Setup
+
+```sh
+npm install
+```
+
+<h4>Compile and Hot-Reload for Development</h4> 
+
+```sh
+npm run dev
+```
+
+<h4>Compile and Minify for Production</h4> 
+
+```sh
+npm run build
+```
+
+<h4>Lint with [ESLint](https://eslint.org/)</h4> 
+
+```sh
+npm run lint
+```
+</details>
---
+++ src/App.vue
@@ -0,0 +1,135 @@
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import { gsap } from "gsap";
+
+let previousScrollY = 0;
+let headerHidden = false;
+
+const handleScroll = () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > previousScrollY && !headerHidden) {
+    headerHidden = true;
+    gsap.to("header", { top: "-200px", duration: 0.3 });
+  } else if (currentScrollY < previousScrollY && headerHidden) {
+    headerHidden = false;
+    gsap.to("header", { top: "0", duration: 0.3 });
+  }
+
+  previousScrollY = currentScrollY;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
+<template>
+  <header>
+    <div class="logo">
+      <img
+        src="/src/assets/images/itsgravytrans-2.png"
+        alt="logo de itsgravy"
+      />
+    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+    <div class="social">
+      <a href="https://www.linkedin.com/in/matthieugravy/"
+        ><img src="/src/assets/images/in.png" alt="linkedin"
+      /></a>
+      <a href="https://github.com/matthieuGravy"
+        ><img src="/src/assets/images/guyteub.png" alt="matthieuGravy"
+      /></a>
+    </div>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped lang="scss">
+header {
+  //background-color: rgb(32, 32, 32);
+  width: 100%;
+  line-height: 1.5;
+  display: flex;
+  flex-direction: row;
+  background-color: black;
+  position: fixed;
+  top: 0;
+  z-index: 10;
+
+  .logo {
+    margin: auto;
+    flex: 1;
+    img {
+      padding-left: 2rem;
+      height: 5vh;
+    }
+  }
+  .social {
+    flex: 1;
+
+    margin: auto;
+    display: flex;
+    flex-flow: row-reverse;
+    padding-right: 2rem;
+    img {
+      margin: auto;
+      padding: 0.5rem;
+      height: 5vh;
+    }
+  }
+  nav {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    a {
+      padding: 1rem 2rem;
+      display: inline-block;
+      border-left: 1px solid var(--color-border);
+      .router-link-exact-active:hover {
+        background-color: transparent;
+      }
+      .router-link-exact-active {
+        color: var(--color-text);
+      }
+      &::first-of-type {
+        border: 0;
+      }
+    }
+  }
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+  }
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
